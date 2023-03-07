7ddd4bbcd06bfb47a7f9b96929d9b148624b7ee3
Jake Romer <jake@jmromer.org>
2023-03-06 21:10:34 -0600

    [be] Vendorize Highcharts

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,8 +2,15 @@
 
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+
+pin "highcharts" # @10.3.3
+pin "highcharts/modules/exporting", to: "highcharts--modules--exporting.js" # @10.3.3
+pin "highcharts/modules/data", to: "highcharts--modules--data.js" # @10.3.3
+pin "highcharts/modules/export-data", to: "highcharts--modules--export-data.js" # @10.3.3
+pin "highcharts/modules/accessibility", to: "highcharts--modules--accessibility.js" # @10.3.3
+
+pin "application", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
